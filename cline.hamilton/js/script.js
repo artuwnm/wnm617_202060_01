3a7a87fac27836e232fa33a0b8376dfa0ff7529f
Hamilton <hamdiggy@gmail.com>
2020-06-29 12:58:20 -0700

    module 3

--- cline.hamilton/js/script.js
+++ cline.hamilton/js/script.js
@@ -12,6 +12,18 @@ $(()=>{
 
 
 	/* TAB GROUP */
+	$(".tabgroup .tab").on("click",function(e){
+		let id = $(this).index();
+
+		$(this).addClass("active")
+			.siblings().removeClass("active")
+
+		$(this).closest(".tabgroup")
+			.find(".content").eq(id)
+				.addClass("active")
+			.siblings().removeClass("active")
+	});
+
 
 });
 
