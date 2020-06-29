1d94d3a845dc8968a0d3fcd63162b21f96d58926
zijianliu-tom <60673009+zijianliu-tom@users.noreply.github.com>
2020-06-29 11:40:16 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+
+$(()=>{
+
+	$(".accordion dt").on("click",function(e){
+		// $(this).next().toggle();
+		// $(this).next().slideToggle();
+		$(this).next().slideDown()
+			.siblings("dd").slideUp();
+	});
+
+});
+
+
+// window.addEventListener("DOMContentLoaded",function(e){
+// 	document.querySelectorAll(".accordion dt").forEach(o=>{
+// 		o.addEventListener("click",function(e){
+// 			o.nextElementSibling.style.display = this.nextElementSibling.style=="none"?"":"none";
+// 		})
+// 	})
+// })
