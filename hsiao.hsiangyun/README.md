19e680544a3d2e1d87fb751b00e5026a83a93033
HsiangYunHsiao <asx13299@gmail.com>
2020-06-30 12:06:29 -0700

    First student commit

---
+++ hsiao.hsiangyun/README.md
@@ -0,0 +1,6 @@
+# Hsiang Yun Hsiao
+
+## Links
+
+- https://hdraws.com
+- https://hdraws.com/aau/wnm617/hsiao.hsiangyun
---
+++ hsiao.hsiangyun/js/script.js
@@ -0,0 +1,15 @@
+//$(()=>{}
+
+//$(".accordion dt").on("click",function(e){
+	console.log("click");
+//});
+//});
+
+window.addEventListener("DOMContentLoaded",function(e){
+	document.querySelectorAll(".accordion dt").forEach(o=>{
+		o.addEventListener(function(e){
+			this.nextElementSibling.
+			
+		})
+	})
+})
