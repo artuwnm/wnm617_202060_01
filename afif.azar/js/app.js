dc4b285d54010047d9bcf34fae073e483c01ee09
azarafif <35200456+azarafif@users.noreply.github.com>
2020-07-12 16:48:12 -0700

    [modal-touchup]

--- afif.azar/js/app.js
+++ afif.azar/js/app.js
@@ -1,33 +1,49 @@
-/*======= UNICORN APP JS==============*/
-
-//////////* Use EMAIL instead of USERNAME (for signin/login) *///////////
 
 $(()=>{
 
-checkUserId();
+	checkUserId();
+
+	// EVENT DELEGATION
+	$(document)
 
 
-//EVENT DELEGATION
-$(document)
-
-// FORM SUBMISSIONS
+	// FORM SUBMISSIONS
 	.on("submit","#signin-form",function(e){
 		e.preventDefault();
 		checkLoginForm();
 	})
 
 
-// CLICKS
+
+
+	// CLICKS
 	.on("click",".js-logout",function(e){
 		sessionStorage.removeItem('userId');
 		checkUserId();
 	})
 
 
-// ACTIVATE TOOLS
-.on("click","[data-activate]",function(e){
-    $($(this).data("activate")).addClass("activate");
-})
+	// ACTIVATE TOOLS
+	.on("click","[data-activate]",function(e){
+		$($(this).data("activate")).addClass("active");
+	})
+	.on("click","[data-deactivate]",function(e){
+		$($(this).data("deactivate")).removeClass("active");
+	})
+	.on("click","[data-toggle]",function(e){
+		$($(this).data("toggle")).toggleClass("active");
+	})
+	.on("click","[data-activateone]",function(e){
+		$($(this).data("activateone")).addClass("active")
+			.siblings().removeClass("active");
+	})
+
+	;
+
+
+
+	$("[data-template]").each(function(){
+		$(this).html($($(this).data("template")).html());
+	})
 
 });
-
