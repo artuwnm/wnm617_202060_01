564b54225e6b298268459666843f9adb79b22759
Jang.Minkyeong <guddussla@gominkyeong.com>
2020-07-14 18:34:46 -0700

    upload file

--- Jang.Minkyeong/js/map.js
+++ Jang.Minkyeong/js/map.js
@@ -2,6 +2,6 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 37.773972, lng: -122.431297 },
-    zoom: 11
+    zoom: 11.5
   });
 }
